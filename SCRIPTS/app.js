f1e666411b60406ec77daa1a54d02a60b93c09eb
padiernas <padierna.suarez@gmail.com>
2022-07-18 09:27:12 -0500

    creamos una constante y vimos variable undefined

---
+++ SCRIPTS/app.js
@@ -0,0 +1,11 @@
+let nombre=prompt ("Ingrese su nombre");
+let apellido=prompt("Ingrese su apellido");
+
+console.log(nombre+apellido);
+//este es una constante
+const email= "sas@gmail.com";
+console.log (`comuniquese maldita perra ${email}`);
+
+//este es un ejemplo de undef
+let correo;
+console.log (`comuniquese maldita perra ${correo}`);
