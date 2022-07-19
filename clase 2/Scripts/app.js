0addd66a2bab2a688d67e1d58d0d17092dea8594
padiernas <padierna.suarez@gmail.com>
2022-07-19 09:12:21 -0500

    orden de importaciones y el uso de var o let

---
+++ clase 2/Scripts/app.js
@@ -0,0 +1,16 @@
+alert("hola mundo")
+
+let firstname= "santiago";
+let lastname= "padierna suarez";
+let gender="male";
+// let age=26;
+
+if (gender=="female") {
+    var age =5;
+}else{
+    console.log(age)
+}
+
+firstname="pepe"
+
+console.log (`Bien venido ${firstname} ${lastname} su genero es ${gender} y su edad es ${age} `)
