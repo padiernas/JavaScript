d01d8679c9d405ce6bf0174e2d9e5781db927179
padiernas <padierna.suarez@gmail.com>
2022-07-21 08:09:55 -0500

    esta es la clase #3 sobre arrays y objetos

---
+++ clase 3/assets/app.js
@@ -0,0 +1,16 @@
+//crear array vacio
+let array=[];
+
+console.log(array);
+
+//vamos a crear 4 vectores con diferentes valores o datos
+let numeros=[1,2,3,4],
+animales=["perro","gato","sapo","loro"],
+folclor=["guaneña","bambuco","pasillo","son jarocho"]
+palabras=["melifluo","serendipia","inmarcesible","bonhomia"]
+
+console.log(numeros);
+console.log(animales);
+console.log(folclor);
+//esto es para saber cuantos vectores hay
+console.log(palabras.length);
