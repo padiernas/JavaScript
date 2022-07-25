167b5d41980fa16b6363b2e38b139243267ec283
padiernas <padierna.suarez@gmail.com>
2022-07-25 09:04:03 -0500

    vimos datos booleanos y operadores de igualdad

---
+++ clase 5/script/index.js
@@ -0,0 +1,10 @@
+let isHungry=true;
+let isSunny=false;
+
+console.log(isHungry);
+console.log(isSunny);
+
+let num1=5, num2='5';
+
+console.log(num1==num2)//valida si son iguales en contenido
+console.log(num1===num2)//valida si son iguales en tipo de dato y contenido
