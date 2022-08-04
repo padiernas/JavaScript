7ac569c80580878d264f6e0fe29d2a9099766835
padiernas <padierna.suarez@gmail.com>
2022-08-04 07:37:27 -0500

    estas son las etiquetas con  nombre

---
+++ clase 10/app.js
@@ -0,0 +1,6 @@
+//Obtener elementos con la funcion getElementsbytagName
+let todosTitulos=document.getElementsByTagName('h1')
+
+console.log(todosTitulos)
+
+let todosSubtitulos=document.getElementsByTagName('h2')
