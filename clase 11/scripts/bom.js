59df202620bd62ccbd8a1731956349b3c568012e
padiernas <padierna.suarez@gmail.com>
2022-08-08 09:08:08 -0500

    vimos todo el tema del bom-Ventana

---
+++ clase 11/scripts/bom.js
@@ -0,0 +1,14 @@
+let anchoVentana=window.innerWidth
+let altoVentana=window.innerHeight
+
+console.log(anchoVentana, altoVentana)
+
+let medidaPantalla=window.screen
+console.log(medidaPantalla)
+
+window.location.pathname
+
+window.location.hostname
+
+window.location.protocol
+
